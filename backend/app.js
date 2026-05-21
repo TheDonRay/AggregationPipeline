5245fc0a4da8a8c31f7de55f384e6bac72968631
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-20 21:29:48 -0400

    setting up project structure

---
+++ backend/app.js
@@ -0,0 +1,15 @@
+import express from 'express'; 
+const app = express(); 
+
+app.use(express.json()); // for middleware 
+ 
+//import routes 
+
+
+app.get('/projectspecs', (req, res) => { 
+    res.json({ 
+        Specs: "Welcome to AGTPipeline. This project is designed to help me learn about aggregation pipeline and create a cool dashboard based of the pipeline"
+    }); 
+});  
+
+module.exports = app; 
