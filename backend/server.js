192ea99a7b231ab2fe13464ae43d75f6a0d12f02
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-22 13:38:49 -0400

    working on the backend

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,11 @@ const PORT = process.env.PORT || 4567;
 //db connection here 
 import dbconnection from './config/db.connection.js'; 
 // import inserting seed data function here 
-dbconnection(); 
-//invoke both the db connection function and the inserting seed data function here 
+import { insertSeedData } from './services/dataexport.services.js'; 
 
+//invoke both the db connection function and the inserting seed data function here 
+dbconnection();  
+insertSeedData();
 
 app.listen(PORT, () => {
   console.log(`Server successfully running at http://localhost:${PORT}`);
