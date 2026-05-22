790cb73108c21f73fbab4da33a70c52965a291c3
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-22 13:02:44 -0400

    added connection

--- backend/config/db.connection.js
+++ backend/config/db.connection.js
@@ -1,3 +1,13 @@
 import {mongoose} from 'mongoose'; 
 const URI = process.env.DBURI;  
 
+const dbconnection = async () => { 
+    try { 
+        await mongoose.connection(URI);  
+        console.log('Connection Successful'); 
+    } catch (err0r) { 
+        console.log('Error connecting to database', error); 
+    }
+}; 
+
+export default dbconnection; 
