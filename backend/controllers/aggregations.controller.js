46cda893eef7d54db9a8a1c460b7705f8834e544
Rayat Chowdhury <rayatchowdhury2005@gmail.com>
2026-05-23 00:07:52 -0400

    aggregations complete

--- backend/controllers/aggregations.controller.js
+++ backend/controllers/aggregations.controller.js
@@ -1,6 +1,48 @@
 import UserData from '../model/db.model.js'; 
 
-export const aggregationController = async (req, res) => { 
+export const averageAge = async (req, res) => {
+    try {
+        const avgAge = await UserData.aggregate([
+            {
+                $group: { _id: null, AverageAge: { $avg: "$age" } }
+            }
+        ]);
+        res.status(200).json({ AggregationResult: avgAge });
+    } catch (error) {
+        console.error('There was an error with this aggregation', error);
+        res.status(500).json({ Error: error });
+    }
+};
+
+export const activeVsInactive = async (req, res) => {
+    try {
+        const comparison = await UserData.aggregate([
+            {
+                $group: { _id: "$isActive", count: { $sum: 1 } }
+            }
+        ]);
+        res.status(200).json({ AggregationResult: comparison });
+    } catch (error) {
+        console.error('There was an error with this aggregation', error);
+        res.status(500).json({ Error: error });
+    }
+};
+
+export const groupByMaritalStatus = async (req, res) => {
+    try {
+        const groupMarital = await UserData.aggregate([
+            {
+                $group: { _id: "$maritalStatus", count: { $sum: 1 } }
+            }
+        ]);
+        res.status(200).json({ AggregationResult: groupMarital });
+    } catch (error) {
+        console.error('There was an error with this aggregation', error);
+        res.status(500).json({ Error: error });
+    }
+};
+
+export const aggregationController = async (req, res) => {
     try { 
         // here we are performing aggregations 
         const exampleAggregation = await UserData.aggregate([ 
@@ -8,7 +50,7 @@ export const aggregationController = async (req, res) => {
                 $match: { profession: /Engineer/}
             }, 
             { 
-                $count: 'numEngineers'
+                $count: 'number of Engineers'
             }
         ]);  
         return res.status(200).json({ 
--- backend/routes/aggregations.route.js
+++ backend/routes/aggregations.route.js
@@ -1,7 +1,10 @@
-import express from 'express'; 
-const aggregationroute = express.Router(); 
-import {aggregationController} from '../controllers/aggregations.controller.js'; 
+import express from 'express';
+const aggregationroute = express.Router();
+import { aggregationController, averageAge, activeVsInactive, groupByMaritalStatus } from '../controllers/aggregations.controller.js';
 
-aggregationroute.get('/dashboard', aggregationController); 
+aggregationroute.get('/numengineers', aggregationController);
+aggregationroute.get('/avgage', averageAge);
+aggregationroute.get('/activestatus', activeVsInactive);
+aggregationroute.get('/maritalstatus', groupByMaritalStatus);
 
-export default aggregationroute; 
+export default aggregationroute;
